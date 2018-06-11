a11694cdc3b0937304f07fb39b0e0c70e4aa4e42
bigcat2014 <ltogan.2014@gmail.com>
2018-06-10 22:01:21 -0400

    Assignment8 finished

---
+++ Assignment8/Assignment8.hs
@@ -0,0 +1,30 @@
+-- 1a
+data BTree a = Empty | Node a (BTree a) (BTree a)
+
+-- 1b
+toList :: BTree a -> [a]
+toList Empty = []
+toList (Node x t1 t2) = (toList t1) ++ [x] ++ (toList t2)
+
+-- 1c
+insert :: Ord a => a -> BTree a -> BTree a
+insert x Empty = (Node x Empty Empty)
+insert x (Node y t1 t2)
+    | x < y     = (Node y (insert x t1) t2)
+    | x > y     = (Node y t1 (insert x t2))
+    | otherwise = (Node y t1 t2)
+
+-- 1d
+fromList :: Ord a => [a] -> BTree a
+fromList xs = foldr insert Empty xs
+
+-- 1e
+sort :: Ord a => BTree a -> BTree a
+sort = fromList . toList
+
+-- 2
+lookupOrd :: Ord a => a -> [(a, b)] -> Maybe b
+lookupOrd y (x:xs)
+    | y == (fst x)  = Just (snd x)
+    | y < (fst x)   = Nothing
+    | otherwise     = lookupOrd y xs
